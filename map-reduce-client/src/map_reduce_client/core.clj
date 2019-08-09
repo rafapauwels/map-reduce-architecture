b0d447e272245603091ba1a84ff9560811c08441
Rafael Pauwels <rafapauwels@gmail.com>
2019-08-09 01:13:57 -0300

    Alterando endereços e criando função de mapeando de urls

--- map-reduce-client/src/map_reduce_client/core.clj
+++ map-reduce-client/src/map_reduce_client/core.clj
@@ -1,5 +1,16 @@
 (ns map-reduce-client.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(defn mapeia-urls
+  "Carrega lista de URLs do FS separado por \n e retorna um mapa no formato 
+  {:url url}, (keyword (re-find #\"(?<=www\\.).*(?=\\.com|\\.edu)\" %))\""
+  [arquivo]
+  (map 
+   #(hash-map 
+     :url
+     %)
+   (str/split-lines (slurp arquivo))))
 
 (defn -main
   "I don't do a whole lot ... yet."
