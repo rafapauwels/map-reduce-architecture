e05f21395b516788137fbbff24c5dd7c62dd8f2c
Rafael Pauwels <rafapauwels@gmail.com>
2019-08-14 01:32:36 -0300

    Finalizado cliente e coordenador

--- map-reduce-coordinator/src/map_reduce_coordinator/core.clj
+++ map-reduce-coordinator/src/map_reduce_coordinator/core.clj
@@ -1,13 +1,33 @@
 (ns map-reduce-coordinator.core
   (:gen-class))
 (import '[java.net DatagramSocket
-          DatagramPacket])
+          DatagramPacket
+          InetSocketAddress])
+
+(defn split-in
+  [n mapa]
+  (->> mapa
+       (map-indexed (fn [i x] [(mod i n) x]))
+       (group-by first)
+       vals
+       (mapv #(mapv peek %))))
+
+(defn send-to-mapper!
+  [data mapper]
+  (let [payload (.getBytes (pr-str data))
+        payload-size (min (alength payload) 512)
+        target (InetSocketAddress. (:url mapper) (:port mapper))
+        package (DatagramPacket. payload payload-size target)]
+    package))
+
+(defn propagate!
+  [splitted-urls mappers socket]
+  (map #(.send socket %) (map send-to-mapper! splitted-urls mappers)))
 
 (defn split-and-propagate
-  [urls mappers]
+  [urls mappers socket]
   (let [n-mappers (count mappers)]
-    (partition-all n-mappers urls) ;; esta separando em grupos de 3 e não em 3 grupos
-    ))
+    (propagate (split-in n-mappers urls) mappers socket)))
 
 (defn receive-urls!
   [^DatagramSocket socket]
@@ -16,15 +36,14 @@
     (.receive socket package)
     (clojure.edn/read-string (String. (.getData package)
                                       0 (.getLength package)))))
-
 (defn loop-receive-urls!
   [f socket mappers]
-  (future (while true (f (receive-urls! socket) mappers))))
+  (future (while true (f (receive-urls! socket) mappers socket))))
 
 (defn -main
   [& args]
   (let [socket (DatagramSocket. 9500)
-        mappers '({:url "localhost" :port 9501}
+        mappers '({:url "localhost" :port 9600}
                   {:url "localhost" :port 9502}
                   {:url "localhost" :port 9503})]
     (loop-receive-urls! split-and-propagate
--- map-reduce-mapper/src/map_reduce_mapper/core.clj
+++ map-reduce-mapper/src/map_reduce_mapper/core.clj
@@ -1,7 +1,30 @@
 (ns map-reduce-mapper.core
   (:gen-class))
+(import '[java.net DatagramSocket
+          DatagramPacket
+          InetSocketAddress])
+
+(defn extract-url!
+  [url]
+  (slurp url))
+
+(defn extract-urls!
+  [urls]
+  (map extract-url urls))
+
+(defn receive-urls!
+  [^DatagramSocket socket]
+  (let [buffer (byte-array 512)
+        package (DatagramPacket. buffer 512)]
+    (.receive socket package)
+    (clojure.edn/read-string (String. (.getData package)
+                                      0 (.getLength package)))))
+(defn loop-receive-urls!
+  [f socket]
+  (future (while true (f (receive-urls! socket) socket))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [socket (DatagramSocket. 9600)]
+    (loop-receive-urls! extract-urls
+                        socket)))
